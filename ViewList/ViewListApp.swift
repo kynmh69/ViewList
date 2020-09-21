004290b67451b623861c0551e34ca6574ea505e6
kynmh69 <hiroki.kayanuma@gmail.com>
2020-09-21 16:12:22 +0900

    initial

---
+++ ViewList/ViewListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewListApp.swift
+//  ViewList
+//
+//  Created by Hiroki Kayanuma on 2020/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
